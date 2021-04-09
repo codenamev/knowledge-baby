4524043d3e55a2e44a10505ebafeb062720d359e
Valentino Stoll <vstoll@doximity.com>
2021-04-09 10:12:31 -0400

    Completion of part-2 of Build a Simple Database (in Rust) – A simple REPL

--- Build A Simple Database/sqliter/src/main.rs
+++ Build A Simple Database/sqliter/src/main.rs
@@ -1,23 +1,75 @@
 use std::io::{self, Write};
 
+enum Command {
+    Exit,
+    NonCommand,
+    Unrecognized
+}
+
+enum StatementType {
+    Insert,
+    Select,
+    Unrecognized
+}
+
+struct Statement {
+    statement_type: StatementType,
+    unprepared: String
+}
+
+impl Command {
+    fn from_str(s: &str) -> Command {
+        match s {
+            command if !command.starts_with(".") => Command::NonCommand,
+            ".exit" => Command::Exit,
+            _ => Command::Unrecognized,
+        }
+    }
+}
+
+impl Statement {
+    fn prepare(s: &str) -> Self {
+        match s {
+            statement if statement.starts_with("insert") => Self { unprepared: statement.to_string(), statement_type: StatementType::Insert },
+            statement if statement.starts_with("select") => Self { unprepared: statement.to_string(), statement_type: StatementType::Select },
+            _ => Self { unprepared: s.to_string(), statement_type: StatementType::Unrecognized },
+        }
+    }
+
+    fn execute(&self) {
+        match self.statement_type {
+            StatementType::Insert => print!("This is where we would do an insert.\n"),
+            StatementType::Select => print!("This is where we would do a select.\n"),
+            StatementType::Unrecognized => print!("Unrecognized keyword at start of \"{}\"\n", self.unprepared.trim()),
+        }
+    }
+}
+
+fn prepare_and_execute_statement(input: &str) {
+    let statement = Statement::prepare(&input);
+
+    statement.execute()
+}
+
 fn print_prompt() {
     print!("db > ");
     let _ = io::stdout().flush();
 }
 
 fn main() {
-    let mut command = String::new();
-
     println!("SQLiter version 0.1.0");
 
     loop {
+        let mut input = String::new();
+
         print_prompt();
 
-        io::stdin().read_line(&mut command).expect("Failed to read line");
+        io::stdin().read_line(&mut input).expect("Failed to read line");
 
-        match command.trim() {
-            ".exit" => break,
-            _ => print!("Unrecognized command: \'{}\' ", command),
+        match Command::from_str(&input.trim().to_lowercase()) {
+            Command::Exit => break,
+            Command::Unrecognized => print!("Unrecognized command: {}", input),
+            Command::NonCommand => prepare_and_execute_statement(&input),
         }
     }
 }
